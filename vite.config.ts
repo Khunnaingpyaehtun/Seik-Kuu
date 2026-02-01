28cbe66a05b10bb889d53568ecba707cbc94b521
Khunnaingpyaehtun <69563773+Khunnaingpyaehtun@users.noreply.github.com>
2026-02-01 10:04:03 +0700

    refine index and json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This allows process.env to be used in the browser code
+    'process.env': process.env
+  },
   build: {
     outDir: 'dist',
   },
